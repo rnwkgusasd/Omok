dbbb8e5e85812932f1b48ea4781ba2f788c2c13a
JaHyun_K <rnwkgusasd@naver.com>
2020-03-26 10:19:40 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Omok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.transparentPanel1 = new Omok.TransparentPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(815, 810);
+            this.label1.TabIndex = 5;
+            // 
+            // transparentPanel1
+            // 
+            this.transparentPanel1.Location = new System.Drawing.Point(96, 9);
+            this.transparentPanel1.Name = "transparentPanel1";
+            this.transparentPanel1.Size = new System.Drawing.Size(815, 810);
+            this.transparentPanel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 827);
+            this.Controls.Add(this.transparentPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private TransparentPanel transparentPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Omok
+{
+    public partial class Form1 : Form
+    {
+        private const int ROW = 19;
+        private const int COLUMN = 19;
+
+        private const int DEFAULT_X = 14;
+        private const int DEFAULT_Y = 14;
+
+        private const int SIZE_WIDTH = 30;
+        private const int SIZE_HEIGHT = 30;
+        private const int SIZE_AREA = SIZE_WIDTH * SIZE_HEIGHT;
+
+        private const int BUTTON_SPACE = 42;
+
+        private Button[,] Board = new Button[ROW, COLUMN];
+
+        private int Count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int tCol = 0; tCol < COLUMN; tCol++)
+            {
+                for(int tRow = 0; tRow < ROW; tRow++)
+                {
+                    Button btn = new Button
+                    {
+                        Location = new Point(DEFAULT_X + (BUTTON_SPACE * tCol), DEFAULT_Y + (BUTTON_SPACE * tRow)),
+                        FlatStyle = FlatStyle.Flat,
+                        Size = new Size(SIZE_WIDTH, SIZE_HEIGHT),
+                    };
+                    btn.BackColor = Color.Orange;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.FlatAppearance.MouseOverBackColor = Color.YellowGreen;
+                    btn.Click += (object s, EventArgs eA) =>
+                    {
+                        Button b = (Button)s;
+
+                        if (b.BackColor == Color.Orange)
+                        {
+                            if (Count % 2 == 0) b.BackColor = Color.Black;
+                            else b.BackColor = Color.White;
+
+                            b.FlatAppearance.MouseOverBackColor = b.BackColor;
+                        }
+
+                        if(WinCheck())
+                        {
+                            MessageBox.Show(Count % 2 == 0 ? "BLACK WIN" : "WHITE WIN");
+                            Application.Exit();
+                        }
+
+                        Count++;
+                    };
+
+                    transparentPanel1.Controls.Add(btn);
+                    Board[tRow, tCol] = btn;
+                }
+            }
+        }
+
+        public bool WinCheck()
+        {
+            Color col = new Color();
+
+            for(int i = 0; i < ROW; i++)
+            {
+                for(int j = 0; j < COLUMN; j++)
+                {
+                    if(Board[i, j].BackColor != Color.Orange)
+                    {
+                        if (NeighborCheck(j, i, Board[i, j].BackColor)) return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool NeighborCheck(int ColIDX, int RowIDX, Color Colr)
+        {
+            if(ColIDX < 0 || ColIDX >= COLUMN || RowIDX < 0 || RowIDX >= ROW)
+                return false;
+
+            bool isOK = false;
+
+            #region Columns Check
+            // right
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX + i >= COLUMN) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX, ColIDX + i].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+
+            // left
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX - i < 0) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX, ColIDX - i].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+            #endregion
+
+            #region Rows Check
+            // down
+            for (int i = 1; i < 5; i++)
+            {
+                if (RowIDX + i >= ROW) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX + i, ColIDX].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+
+            // up
+            for (int i = 1; i < 5; i++)
+            {
+                if (RowIDX - i < 0) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX - i, ColIDX].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+            #endregion
+
+            #region Side up Check
+            // left up
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX - i < 0 || RowIDX - i < 0) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX - i, ColIDX - i].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+
+            // left down
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX - i < 0 || RowIDX + i >= ROW) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX + i, ColIDX - i].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+
+            // right up
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX + i >= COLUMN || RowIDX - i < 0) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX - i, ColIDX + i].BackColor)
+                {
+                    if (i == 4) isOK = true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+
+            // right down
+            for (int i = 1; i < 5; i++)
+            {
+                if (ColIDX + i >= COLUMN || RowIDX + i >= ROW) { isOK = false; break; }
+
+                if (Board[RowIDX, ColIDX].BackColor == Board[RowIDX + i, ColIDX + i].BackColor)
+                {
+                    if (i == 4) return true;
+                    else continue;
+                }
+                else { isOK = false; break; }
+            }
+            #endregion
+
+            return isOK;
+        }
+    }
+
+    public class TransparentPanel : Panel
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return cp;
+            }
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //base.OnPaintBackground(e);
+        }
+    }
+}
